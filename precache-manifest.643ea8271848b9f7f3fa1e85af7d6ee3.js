e993be03adb6975d3e2b463909287ec23c5b90bf
Issam Ait Ouahmane <49284629+issam1994@users.noreply.github.com>
2019-04-09 03:00:24 +0100

    Added files
    all files were added!

---
+++ precache-manifest.643ea8271848b9f7f3fa1e85af7d6ee3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "54f6a74e7970d2314154",
+    "url": "/static/js/main.c798e554.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "54f6a74e7970d2314154",
+    "url": "/static/css/main.646a277a.chunk.css"
+  },
+  {
+    "revision": "fa3ed68b8e7599ee5bb5862678850d77",
+    "url": "/index.html"
+  }
+];
